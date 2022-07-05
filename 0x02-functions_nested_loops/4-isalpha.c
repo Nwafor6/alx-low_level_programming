56ecaf473fddb5552afdae233b1794e55bc82729
Glory <nwaforglory6@gmail.com>
2022-07-05 12:22:04 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isalpha - check for alphabetic letter
+* @c : character to check
+* Return: 0 or 1
+*/
+
+int _isalpha(int c)
+{
+return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+
